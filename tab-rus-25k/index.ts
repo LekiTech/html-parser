f2cdeb4ee8938d00481be2cf61377fecbc251d8f
ktadzjibov <ktadzjibov@gmail.com>
2022-02-17 15:00:02 +0100

    hotfix: indent issue in index.ts

--- tab-rus-25k/index.ts
+++ tab-rus-25k/index.ts
@@ -3,35 +3,35 @@ import { parseAllPages } from '../htmlReader';
 
 
 function htmlPageParser() {
-	return [...document.querySelectorAll('span')]
-		.map(el => {
-				const allStyles = window.getComputedStyle(el);
-				const isUpperCase = el.innerText === el.innerText.toUpperCase() &&
-						el.innerText !== el.innerText.toLowerCase();
-				return { 
-						text: el.innerText,
-						isUpperCase,
-						style: {
-								fontFamily: allStyles.fontFamily,
-								fontSize: allStyles.fontSize,
-								left: allStyles.left,
-								bottom: allStyles.bottom
-						}
-				}
-		});
+  return [...document.querySelectorAll('span')]
+    .map(el => {
+        const allStyles = window.getComputedStyle(el);
+        const isUpperCase = el.innerText === el.innerText.toUpperCase() &&
+            el.innerText !== el.innerText.toLowerCase();
+        return { 
+            text: el.innerText,
+            isUpperCase,
+            style: {
+                fontFamily: allStyles.fontFamily,
+                fontSize: allStyles.fontSize,
+                left: allStyles.left,
+                bottom: allStyles.bottom
+            }
+        }
+    });
 }
 
 function postProcessing(parsedvalues: any[]) {
-	// TODO: process values to get final result
-	return parsedvalues;
+  // TODO: process values to get final result
+  return parsedvalues;
 }
 
 const sourceDirPath = path.join(__dirname, 'dictionary');
 const resultDirPath = path.join(__dirname, 'result');
 (async () => {
-	try {
-			await parseAllPages(sourceDirPath, resultDirPath, htmlPageParser, postProcessing, true);
-	} catch (err) {
-			console.error(err)
-	}
+  try {
+      await parseAllPages(sourceDirPath, resultDirPath, htmlPageParser, postProcessing, true);
+  } catch (err) {
+      console.error(err)
+  }
 })()
